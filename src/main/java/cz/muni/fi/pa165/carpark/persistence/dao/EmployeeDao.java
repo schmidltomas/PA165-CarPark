86a489af00a06d9cc721139aff5510d339e19019
Jakub Kriz <jakobkriz@seznam.cz>
2016-10-28 17:32:17 +0200

    User a Employee bez propojení s db

---
+++ src/main/java/cz/muni/fi/pa165/carpark/persistence/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.pa165.carpark.persistence.dao;
+
+import cz.muni.fi.pa165.carpark.persistence.entity.Employee;
+import java.util.List;
+
+public interface EmployeeDao extends UserDao {
+        
+    
+    public void create(Employee employee);
+    
+    public void update(Employee employee);
+
+    public void delete(Employee employee);
+
+    public Employee findById(String id);
+    
+    public List<Employee> findByName(String firstName, String secoundName);
+
+    public List<Employee> findAll();
+}
