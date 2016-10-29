ccc84d702238c3e43f4a159bd44a244dc4892b82
taptikrobert <rtaptik@gmail.com>
2016-10-30 01:46:18 +0200

    reservations

--- carpark-persistence/src/main/java/cz/muni/fi/pa165/carpark/persistence/dao/ReservationDao.java
+++ carpark-persistence/src/main/java/cz/muni/fi/pa165/carpark/persistence/dao/ReservationDao.java
@@ -1,4 +1,49 @@
 package cz.muni.fi.pa165.carpark.persistence.dao;
 
+import cz.muni.fi.pa165.carpark.persistence.entity.Reservation;
+
+import java.util.List;
+
+/**
+ * Interface for managing reservations in Car Park.
+ *
+ * @author Robert Taptik
+ */
 public interface ReservationDao {
+
+    /** Creates a reservation entity in the database.
+     *
+     * @param reservation to be created, must not be null
+     * @return long reservation id
+     */
+    long create(Reservation reservation);
+
+    /**
+     * Finds a reservation entity in the database by id.
+     *
+     * @param id, must not be null
+     * @return Reservation object or null if not found
+     */
+    Reservation findById(long id);
+
+    /**
+     * Updates a reservation entity in the database.
+     *
+     * @param reservation to be updated, must not be null
+     */
+    void update(Reservation reservation);
+
+    /**
+     * Deletes a reservation entity in the database.
+     *
+     * @param reservation to be deleted, must not be null
+     */
+    void delete(Reservation reservation);
+
+    /**
+     * Lists all the reservations in the database.
+     *
+     * @return List of all the reservation entities in the database
+     */
+    List<Reservation> getAll();
 }
--- carpark-persistence/src/main/java/cz/muni/fi/pa165/carpark/persistence/entity/Reservation.java
+++ carpark-persistence/src/main/java/cz/muni/fi/pa165/carpark/persistence/entity/Reservation.java
@@ -1,4 +1,132 @@
 package cz.muni.fi.pa165.carpark.persistence.entity;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ * Reservation entity
+ *
+ * @author Robert Taptik
+ */
+
+@Entity
+@Table(name = "reservation")
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    private Employee employee;
+
+    @Column
+    private Car car;
+
+    @Column
+    private Date startDate;
+
+    @Column
+    private Date endDate;
+
+    @Column
+    private long distance;
+
+    @Column
+    private String purpose;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public long getDistance() {
+        return distance;
+    }
+
+    public void setDistance(long distance) {
+        this.distance = distance;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reservation)) return false;
+
+        Reservation that = (Reservation) o;
+
+        if (getId() != that.getId()) return false;
+        if (getDistance() != that.getDistance()) return false;
+        if (getEmployee() != null ? !getEmployee().equals(that.getEmployee()) : that.getEmployee() != null)
+            return false;
+        return getCar() != null ? getCar().equals(that.getCar()) : that.getCar() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (getId() ^ (getId() >>> 32));
+        result = 31 * result + (getEmployee() != null ? getEmployee().hashCode() : 0);
+        result = 31 * result + (getCar() != null ? getCar().hashCode() : 0);
+        result = 31 * result + (int) (getDistance() ^ (getDistance() >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", employee=" + employee +
+                ", car=" + car +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", distance=" + distance +
+                ", purpose='" + purpose + '\'' +
+                '}';
+    }
 }
