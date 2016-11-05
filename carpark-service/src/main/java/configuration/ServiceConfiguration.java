5c5c5690bc745bce66afdc84ebbb4870466bff80
Karel Fajkus <karelfajkus1@gmail.com>
2016-11-05 15:52:43 +0100

    added module structure of next milestone

---
+++ carpark-service/src/main/java/configuration/ServiceConfiguration.java
@@ -0,0 +1,10 @@
+package configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by karelfajkus on 05/11/2016.
+ */
+@Configuration
+public class ServiceConfiguration {
+}
